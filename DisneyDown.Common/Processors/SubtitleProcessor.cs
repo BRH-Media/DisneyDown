dbf7bcc200a0d52858a2e5e75741b24a5bd5909d
Baeleigh Harris <baeleighharris@outlook.com>
2021-01-18 21:19:47 +1000

    Subtitle processor reformatting

--- DisneyDown.Common/Processors/SubtitleProcessor.cs
+++ DisneyDown.Common/Processors/SubtitleProcessor.cs
@@ -8,21 +8,38 @@ namespace DisneyDown.Common.Processors
 {
     public static class SubtitleProcessor
     {
+        /// <summary>
+        /// Determines if the current WebVTT line is a timecode line (no settings)
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         private static bool IsTimecode(string line)
+
+            //timecode lines always have this unique string
             => line.Contains("-->");
 
-        public static string DeleteCueSettings(string line)
+        private static string DeleteCueSettings(string line)
         {
+            //cleaned line output
             var output = new StringBuilder();
+
+            //go through each character in the supplied WebVTT line
             foreach (var ch in line)
             {
+                //convert the character to lowercase
                 var chLower = char.ToLower(ch);
 
+                //if the character is in the alphabetical unicode range (061-122)
                 if (chLower >= 'a' && chLower <= 'z')
+
+                    //break the loop; character is alphabetical
                     break;
 
+                //character is not alphabetical; append to the buffer
                 output.Append(ch);
             }
+
+            //return the final cleaned line
             return output.ToString();
         }
 
@@ -30,67 +47,104 @@ namespace DisneyDown.Common.Processors
         {
             try
             {
+                //converted SubRip Text (SRT) lines are stored here
                 var output = new List<string>();
+
+                //current WebVTT line for SubRip timecode counting
                 var lineNumber = 1;
 
+                //go through each line in the WebVTT array
                 for (var i = 0; i < fileLines.Count; i++)
                 {
+                    //the current line
                     var line = fileLines[i];
+
+                    //is this the final iteration?
                     var endOfStream = i == fileLines.Count - 1;
 
+                    //only operate on the line if it's a valid timecode line
                     if (IsTimecode(line))
                     {
+                        //add the current line number for SRT
                         output.Add(lineNumber.ToString());
+
+                        //increment the current line
                         lineNumber++;
+
+                        //convert WebVTT timecode styling to SRT
                         line = line?.Replace('.', ',');
+
+                        //remove all Cue settings/styling information (if any)
                         line = DeleteCueSettings(line);
+
+                        //add the new timecode
                         output.Add(line);
 
+                        //whether an associated caption was found for the current timecode
                         var foundCaption = false;
 
+                        //a copy of the current array index in order to allow for modification
                         var n = i;
+
+                        //endless loop until a break condition is satisfied
                         while (true)
                         {
+                            //is this the final line iteration in the WebVTT file?
                             if (endOfStream)
-                            {
-                                if (foundCaption)
-                                    break;
-                            }
 
+                                //have we already found a caption for this timecode?
+                                if (foundCaption)
+
+                                    //condition satisfied; exit loop
+                                    break;
+
+                            //advance the current line by 1
                             line = fileLines[n + 1];
+
+                            //increment the line index counter
                             n++;
-                            if (string.IsNullOrEmpty(line) || string.IsNullOrWhiteSpace(line))
+
+                            //ensure the line isn't null or whitespace (tabs, spaces, etc.)
+                            if (string.IsNullOrWhiteSpace(line))
                             {
+                                //new empty line to separate captions in the resulting SRT file
                                 output.Add(@"");
+
+                                //condition satisfied; exit loop
                                 break;
                             }
 
+                            //timecode caption found
                             foundCaption = true;
+
+                            //add the resulting line to the buffer
                             output.Add(line);
                         }
                     }
                 }
 
-                // time repair
+                //go through each line in the output buffer
                 for (var i = 0; i < output.Count; i++)
                 {
+                    //make sure we're operating on a valid timecode
                     if (IsTimecode(output[i]))
                     {
-                        var secondTime = output[i].Substring(output[i].IndexOf('>') + 2,
+                        //parse out the end time
+                        var endTime = output[i].Substring(output[i].IndexOf('>') + 2,
                             output[i].LastIndexOf(",", StringComparison.Ordinal) - (output[i].IndexOf('>') + 2));
                         try
                         {
                             var oldLine = output[i];
 
-                            var ts = TimeSpan.Parse(secondTime);
+                            var ts = TimeSpan.Parse(endTime);
 
-                            if (secondTime != ts.ToString())
+                            if (endTime != ts.ToString())
                                 output[i] = oldLine.Replace("--> ", "--> 00:");
 
-                            var firstTime = output[i].Substring(0, output[i].IndexOf(",", StringComparison.Ordinal));
-                            var ts2 = TimeSpan.Parse(firstTime);
+                            var startTime = output[i].Substring(0, output[i].IndexOf(",", StringComparison.Ordinal));
+                            var ts2 = TimeSpan.Parse(startTime);
 
-                            if (firstTime != ts2.ToString())
+                            if (startTime != ts2.ToString())
                                 output[i] = output[i].Replace(output[i], "00:" + output[i]);
                         }
                         catch (Exception)
