a04ae4cd9b3a5711e5087eba2742d54f498c8522
Baeleigh Harris <baeleighharris@outlook.com>
2021-02-09 12:32:46 +1000

    Started work on FFMPEG download utility

---
+++ DisneyDown.Common/Util/ToolGrab/FFMpegGrab.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+namespace DisneyDown.Common.Util.ToolGrab
+{
+    public static class FFMpegGrab
+    {
+        public static string DownloadUrl { get; } = @"https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip";
+        public static string DownloadHash { get; } = @"https://www.gyan.dev/ffmpeg/builds/sha256-release-essentials-zip";
+    }
+}
