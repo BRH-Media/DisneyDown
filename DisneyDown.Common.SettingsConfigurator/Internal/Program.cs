9a0cd43612d94dd6f03cc4531eb3cae00409e2e9
baeleighharris <baeleighharris@outlook.com>
2021-12-26 20:33:53 +1000

    Settings window

---
+++ DisneyDown.Common.SettingsConfigurator/Internal/Program.cs
@@ -0,0 +1,20 @@
+﻿using DisneyDown.Common.SettingsConfigurator.UI;
+using System;
+using System.Windows.Forms;
+
+namespace DisneyDown.Common.SettingsConfigurator.Internal
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Home());
+        }
+    }
+}
---
+++ DisneyDown.Common.SettingsConfigurator/UI/Home.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DisneyDown.Common.SettingsConfigurator.UI
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void LstSettingsSections_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //convert the selected item to a string
+            var settingsSection = lstSettingsSections.SelectedItem.ToString();
+
+            //what do we do?
+            switch (settingsSection)
+            {
+
+            }
+        }
+
+        private void ItmApplySettings_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ItmRevertSettings_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            //first settings profile is always selected by default
+            if (lstSettingsSections.Items.Count > 0)
+            {
+                //this is done to ensure that the user always sees loaded settings
+                //when it is loaded up; otherwise, it's an unnecessary extra step for
+                //the user.
+                lstSettingsSections.SelectedIndex = 0;
+            }
+        }
+    }
+}
